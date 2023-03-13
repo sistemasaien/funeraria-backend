59cae84d9ff683b6243033533f7e4783344584b5
Cristhian Saavedra <csaavedra@topazevolution.com>
2023-03-13 13:36:22 -0300

    nuevos ajustes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'mysql-cristhiansaavedra.alwaysdata.net',
-    user: '243228',
-    password: 'fronterizo',
-    database: 'cristhiansaavedra_funeraria'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
 
 connection.connect(function (err) {
