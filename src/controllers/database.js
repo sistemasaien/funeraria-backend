76970bc6dfedbfa19615f979ea5123ed26591cb5
Cristhian Saavedra <cristhiansaavedra1996@gmail.com>
2023-05-05 21:44:21 -0300

    Update database.js

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -1,27 +1,22 @@
 const mysql = require('mysql');
 
-let connection;
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    keepAlive: true
+});
 
-function connect() {
-    pool = mysql.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        keepAlive: true
-    });
+//getConnection
+connection = pool.getConnection(function (err, connection) {
+    if (err) {
+        connection.release();
+        console.error('Error al conectarse a la base de datos:', err);
+        setTimeout(connect, 2000); // intenta reconectar después de 2 segundos
+    } else {
+        console.log('Conexión establecida con la base de datos');
+    }
+});
 
-    //getConnection
-    connection = pool.getConnection(function (err, connection) {
-        if (err) {
-            console.error('Error al conectarse a la base de datos:', err);
-            setTimeout(connect, 2000); // intenta reconectar después de 2 segundos
-        } else {
-            console.log('Conexión establecida con la base de datos');
-        }
-    });
-}
-
-connect();
-
-module.exports = connection;
+module.exports = pool;
