2996d9429ef0ec8ebed20126d14bbf46ac1a4fee
Cristhian Saavedra <45241687+cristhiansaavedra96@users.noreply.github.com>
2023-09-18 01:14:38 -0300

    Se agrega el puerto

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
 
+//allowPublicKeyRetrieval: true
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     password: process.env.DB_PASSWORD || '',
     user: process.env.DB_USER || 'root',
     database: process.env.DB_DATABASE || 'db',
+    port: process.env.DB_PORT || 3306,
 });
 
 //getConnection
