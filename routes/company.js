799e5656b71760a7f25dfe3eb8a81f12adc57357
Cristhian Saavedra <45241687+cristhiansaavedra96@users.noreply.github.com>
2024-03-24 22:11:03 -0300

    Update company.js

--- routes/company.js
+++ routes/company.js
@@ -2,7 +2,7 @@ const companyController = require('../controllers/company');
 const router = require('express').Router();
 
 router.get('/company', companyController.getCompanyData);
-router.post('/updateCompany', companyController.updateCompanyData);
-router.post('/createCompany', companyController.createCompanyData);
+router.post('/updateCompanyData', companyController.updateCompanyData);
+router.post('/createCompanyData', companyController.createCompanyData);
 
 module.exports = router;
