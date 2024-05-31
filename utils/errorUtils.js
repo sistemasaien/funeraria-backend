47ffd6805992213f03a83e9985b587d7baeace9b
Cristhian Saavedra <45241687+cristhiansaavedra96@users.noreply.github.com>
2024-05-31 18:54:15 -0300

    Update errorUtils.js

--- utils/errorUtils.js
+++ utils/errorUtils.js
@@ -9,16 +9,22 @@ const logError = (error) => {
     const logFilePath = logsDir + logFileName;
 
     let basePath = global.__basedir;
-    //sustituir \ por \\
+    // Sustituir \ por \\
     basePath = basePath.replace(/\\/g, '\\\\');
     let formattedError = JSON.stringify(error, null, 2)
-        .replaceAll('\\node_modules', '\\pode_modules') // Reemplazar \node_modules por \pode_modules (para que no se interprete como un salto de línea
+        .replaceAll('\\node_modules', '\\pode_modules') // Reemplazar \node_modules por \pode_modules para que no se interprete como un salto de línea
         .replaceAll(/\\n/g, '\n') // Reemplazar \n por saltos de línea
         .replaceAll('\\pode_modules', '\\node_modules')
-        .replaceAll(basePath, '') // Reemplazar la ruta base del proyecto por una cadena vacía
+        .replaceAll(basePath, ''); // Reemplazar la ruta base del proyecto por una cadena vacía
 
     const logMessage = `${now.format('HH:mm:ss')} : ${formattedError}\n`;
+
     try {
+        // Verificar si el directorio de logs existe, si no, crearlo
+        if (!fs.existsSync(logsDir)) {
+            fs.mkdirSync(logsDir, { recursive: true });
+        }
+
         // Verificar si el archivo de registro ya existe
         if (fs.existsSync(logFilePath)) {
             // Si el archivo existe, agregar el mensaje de error al final
@@ -27,6 +33,7 @@ const logError = (error) => {
             // Si el archivo no existe, crearlo y agregar el mensaje de error
             fs.writeFileSync(logFilePath, logMessage);
         }
+
         console.log('Error registrado en el archivo de logs:', logFileName);
     } catch (err) {
         console.error('Error al registrar el error en el archivo de logs:', err);
